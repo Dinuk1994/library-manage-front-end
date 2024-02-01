abbfdf247adbcadf67bec080d62c733be96d55c4
Dinuk1994 <ddt941119@gmail.com>
2024-02-01 21:05:02 +0530

    library-manage-FE

---
+++ src/app/page/view-all-books/view-all-books.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-view-all-books',
+  templateUrl: './view-all-books.component.html',
+  standalone: true,
+  imports: [HttpClientModule,FormsModule,CommonModule],
+  styleUrls: ['./view-all-books.component.css']
+})
+export class ViewAllBooksComponent implements OnInit {
+  private http: any;
+  public bookList: any = {};
+
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+
+
+    this.http.get('http://localhost:8080/book/get').subscribe((data: any) => {
+      this.bookList = data;
+      console.log(this.bookList);
+    });
+
+  }
+}
